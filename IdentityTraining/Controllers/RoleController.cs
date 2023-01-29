cb4ca8a44d4393ecd371b67928f715415e86ad27
Didic Victor <75629328+DidicV@users.noreply.github.com>
2023-01-29 17:34:47 +0200

    Role view & controller

---
+++ IdentityTraining/Controllers/RoleController.cs
@@ -0,0 +1,22 @@
+﻿using IdentityTraining.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityTraining.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RoleController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _context.Roles.OrderBy(r => r.Name).ToList();
+
+            return View(roles);
+        }
+    }
+}
